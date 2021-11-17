d48f63579b4012e7baceabc4ae683af19ac23791
Cristiano Battistini <cristianobattistini09@gmail.com>
2021-11-17 15:33:16 +0100

    create spring project

---
+++ learning/src/test/java/it/cristiano/learning/LearningApplicationTests.java
@@ -0,0 +1,13 @@
+package it.cristiano.learning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
